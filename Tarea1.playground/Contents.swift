ea1d714ff369f3befd545d98631d3faad40952b4
Harold Arias <harold_joseph_07@hotmail.com>
2021-03-18 13:49:50 -0500

    Commit

---
+++ Tarea1.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+// Arrays
+
+func numeroPar(_ number:Int) -> Bool {
+    return number % 2 == 0
+}
+
+var arrayNumber = [Int]()
+
+arrayNumber += 1...100
+
+print("PAR E IMPAR CON ARRAY")
+print("")
+
+//For par e impar con array
+for i in arrayNumber {
+    if numeroPar(i) {
+        print("Es par: " , i)
+    }else {
+        print("Es impar: " , i)
+    }
+}
+
+XD ESTA ES LA TAREA :V APUNTA NOMAS :V capturasoXD SIP
